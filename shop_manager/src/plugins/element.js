f6296cb8a7c153446e3d592d4efe4fdf0a86e24a
zll <swizll@163.com>
2020-03-31 09:20:17 +0800

    登录以及等处功能的实现

--- shop_manager/src/plugins/element.js
+++ shop_manager/src/plugins/element.js
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import { 
     Button, 
     Radio,
-    Row
+    Row,
+    Form,
+    FormItem,
+    Input,
+    Message
 } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Radio)
 Vue.use(Row)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+//将message组件挂载到VUE对象，作为全局属性
+Vue.prototype.$msg=Message
--- shop_manager/src/router/index.js
+++ shop_manager/src/router/index.js
@@ -17,6 +17,21 @@ const routes = [
   //   // which is lazy-loaded when the route is visited.
   //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   // }
+  // 按需导入 需要使用该路由组建时 才带入
+  {
+    path:'/',
+    redirect:'/login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/Login.vue')
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: () => import('@/views/Home.vue')
+  }
 ]
 
 const router = new VueRouter({
@@ -24,5 +39,19 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
-
+// 添加路由守卫  beforeEach导航守卫
+// to  表示将要访问你的路径 form 从哪来  next 是下一个要进行的操作
+router.beforeEach((to,from,next)=>{
+  //对用户访问的路径进行拦截，如果是登录页面，则直接放行
+  if(to.path==='/login'){
+    return next()
+  }
+  //如果访问的不是登录页 则我们需要来检测用户是否登陆 如果登陆 则直接放行  否则 就跳转到登录页
+  const token =window.sessionStorage.getItem('token')
+  if(!token){
+    //没有token
+    return next('/login')
+  }
+  next()
+})
 export default router
